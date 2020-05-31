9c5c295f3ae77d909d70c0306637f7c01351aee1
Adrián Arcalá <aarcala10@gmail.com>
2020-05-31 05:03:47 +0200

    Añadimos animación a los userAvatar para cada vez que son obtenidos de la API

--- DiscourseClient/Topics/TopicCell.swift
+++ DiscourseClient/Topics/TopicCell.swift
@@ -47,11 +47,31 @@ class TopicCell: UITableViewCell {
         
     }
     
+    private func animate(){
+        let shapeLayer = CAShapeLayer()
+        let squarePath = UIBezierPath(rect: avatarUser.bounds).cgPath
+        shapeLayer.path = squarePath
+        shapeLayer.fillColor = .init(srgbRed: 50, green: 50, blue: 50, alpha: 0.6)
+        shapeLayer.contents = avatarUser.image
+        avatarUser.layer.addSublayer(shapeLayer)
+
+        let morphAnimation = CABasicAnimation(keyPath: "path")
+        morphAnimation.fromValue = squarePath
+        let circlePath = UIBezierPath(ovalIn: avatarUser.bounds).cgPath
+        morphAnimation.toValue = circlePath
+        morphAnimation.duration = 1.0
+
+        shapeLayer.speed = 0.6
+        shapeLayer.add(morphAnimation, forKey: "morphAnimation")
+        
+        shapeLayer.path = .none
+    }
+    
 }
 extension TopicCell: TopicCellViewModelViewDelegate {
     func userImageFetched() {
         avatarUser.image = viewModel?.avatarUser
-        
+        animate()
         setNeedsLayout()
     }
 }
--- DiscourseClient/Users/UserCell.swift
+++ DiscourseClient/Users/UserCell.swift
@@ -13,7 +13,6 @@ class UserCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameView: UILabel!
     
-    
     var viewModel: UserCellViewModel? {
         didSet {
             guard let viewModel = viewModel else { return }
@@ -29,11 +28,33 @@ class UserCell: UICollectionViewCell {
         nameView.tintColor = .black
     
     }
+    
+    private func animate(){
+        let squarePath = UIBezierPath(rect: imageView.bounds).cgPath
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = squarePath
+        shapeLayer.fillColor = .init(srgbRed: 50, green: 50, blue: 50, alpha: 0.6)
+        shapeLayer.contents = imageView.image
+        imageView.layer.addSublayer(shapeLayer)
+
+        let morphAnimation = CABasicAnimation(keyPath: "path")
+        morphAnimation.fromValue = squarePath
+        let circlePath = UIBezierPath(ovalIn: imageView.bounds).cgPath
+        morphAnimation.toValue = circlePath
+        morphAnimation.duration = 1.0
+
+        shapeLayer.speed = 0.6
+        shapeLayer.add(morphAnimation, forKey: "morphAnimation")
+        
+        shapeLayer.path = .none
+    }
+    
 }
 
 extension UserCell: UserCellViewModelViewDelegate {
     func userImageFetched() {
         imageView?.image = viewModel?.userImage
+        animate()
         setNeedsLayout()
     }
 }
